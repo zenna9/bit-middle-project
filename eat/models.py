5ae988ee8b3cf5d9146d74834c48a139249dee20
zenna9 <cheueun3@gmail.com>
2022-04-08 18:21:00 +0900

    today done

--- eat/models.py
+++ eat/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class login(models.Model):
+    usr_id = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    usr_name = models.CharField(max_length=10)
+    usr_sex = models.CharField(max_length=2)
+    usr_height = models.IntegerField(default=0)
+    user_weight = models.FloatField(default=0.0)
+
+class diet(models.Model) :
+    date = models.DateField
+    time = models.CharField(max_length=10)
+    kcal = models.FloatField(default=0.0)
+    tan = models.FloatField(default=0.0)
+    dang = models.FloatField(default=0.0)
+    ji = models.FloatField(default=0.0)
+    dan = models.FloatField(default=0.0)
+    kalsum = models.FloatField(default=0.0)
+    inn = models.FloatField(default=0.0)
+    salt = models.FloatField(default=0.0)
+    kalum = models.FloatField(default=0.0)
+    magnesum = models.FloatField(default=0.0)
+    chul = models.FloatField(default=0.0)
+    ayeon = models.FloatField(default=0.0)
+    kolest = models.FloatField(default=0.0)
+    transfat = models.FloatField(default=0.0)
--- eat/views.py
+++ eat/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def upload_p(request):
+    return render(request, 'your_photo.html')
