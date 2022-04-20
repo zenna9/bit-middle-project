0218266843dfb1c0b6e6a025c6e1e0b9292d12a3
zenna9 <cheueun3@gmail.com>
2022-04-20 17:38:35 +0900

    22.04.20 05-38

--- analysis_photo/urls.py
+++ analysis_photo/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from analysis_photo import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 # url/ph/
 
-app_name = 'analysis'
+app_name = 'analysis_photo'
 
 urlpatterns = [
-    path('your_photo/', views.your_photo, name='your_photo'),
+    path('', views.uploadFile, name='uploadFile'),
+    path('<int:document_id>/', views.upload2, name='upload2')
 ]
+
+if settings.DEBUG : 
+    urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root = settings.MEDIA_ROOT
+    )
--- analysis_photo/views.py
+++ analysis_photo/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from eat import models
+from eat.models import imgs
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-def your_photo(request):
-    return render(request, 'your_photo.html')
+def uploadFile(request):
 
+    if request.method =="POST":
+        #폼에서 데이터 받아오기
+        uploadedFile = request.FILES["uploadedFile"]
+        zenna_url = ""
+        #폴더에 저장
+        document = models.imgs(
+            uploadedFile = uploadedFile
+        )
+        document.save()
+        zenna_url =  document.uploadedFile
+        print("zenna 이거야", zenna_url)
+    document = get_object_or_404(imgs, uploadedFile=zenna_url)
+    document_id = document.id
+    return HttpResponseRedirect(reverse('analysis_photo:upload2', args=(document_id,)))
+
+def upload2(request, document_id):
+    one_image_all_info = get_object_or_404(imgs, id=document_id)
+    context = {'oiai': one_image_all_info}
+    return render(request, 'your_photo.html', context)
