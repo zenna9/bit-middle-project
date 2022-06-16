188caa3b6894a313c89cf24db84cd83f16f4b450
zenna <97446606+zenna9@users.noreply.github.com>
2022-06-16 17:18:40 +0900

    튜토리얼부분 약간 손댄부분있고 + mysql.py파일 리눅스용으로 수정해서 올림

--- whateat/mysql.py
+++ whateat/mysql.py
@@ -2,18 +2,18 @@
 class oursql():
 
     # =============AWS환경 사용 시================
-    # serverhost = ['43.200.16.33',"*"]
-    # s_host = 'ec2-43-200-16-33.ap-northeast-2.compute.amazonaws.com'
-    # s_port = 3306
-    # s_user = 'user1'
-    # s_passwd='1111'
-
-    # =============local환경 사용 시==============
-    serverhost = ['*', 'localhost']
-    s_host = '192.168.0.29'
+    serverhost = ['43.200.16.33',"*"]
+    s_host = 'ec2-43-200-16-33.ap-northeast-2.compute.amazonaws.com'
     s_port = 3306
     s_user = 'user1'
     s_passwd='1111'
+
+    # =============local환경 사용 시==============
+    # serverhost = ['*', 'localhost']
+    # s_host = '192.168.0.29'
+    # s_port = 3306
+    # s_user = 'user1'
+    # s_passwd='1111'
 
     # =============채은 재택근무==================
     # serverhost = ['*', 'localhost']
