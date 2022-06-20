dbb964d82a6071235d31a9278f1fbc6b1d749667
Kyeong Jun <rhrudwnszoq@naver.com>
2022-06-20 14:27:39 +0900

    0620 경준 웹화면에서 날짜별사진보기 js오류 해결

--- static/js/kj.js
+++ static/js/kj.js
@@ -3,3 +3,9 @@ function change_cal(idx){
     var cal_date = document.getElementById('kj_calender').value;
     window.location.pathname = 'm/ain/'+ cal_date +'/'+ 'mypage';
 }
+
+function change_cal1(idx){
+    console.log(idx);
+    var cal_date = document.getElementById('kj_calender1').value;
+    window.location.pathname = 'm/ain/'+ cal_date +'/'+ 'mypage';
+}
